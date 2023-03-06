ec22dd7eab756d6410acff92242c135ddc6e2ace
marlonvess <marlonvess@gmail.com>
2023-03-06 18:00:02 +0000

    Game playable via browser console w/o scoreboard

--- javascript.js
+++ javascript.js
@@ -1,2 +1,36 @@
-const options = ["rock","paper","scissors","lizard","spock"];
-console.log (options);
+//CPU random Choices 
+function getComputerChoice () {
+ const cpuChoice = parseInt((Math.random() * 5));
+ return cpuChoice 
+}
+
+const cpuNumber = getComputerChoice();
+
+// 0 = Rock     1 = Paper   2 = Scissors    3 = Lizard      4 = Spock
+
+function playRound (cpuNumber, playerSelection) {
+    if (playerSelection == cpuNumber) {
+        return `Empate!`;
+    } 
+    else if (playerSelection == 0 && (cpuNumber == 2 || cpuNumber == 3)) {
+        return `Venceu1!`;
+    }
+    else if (playerSelection == 1 && (cpuNumber == 4 || cpuNumber == 0)) {
+        return `Venceu2!`;
+    }
+    else if (playerSelection == 2 && (cpuNumber == 1 || cpuNumber == 3)) {
+        return `Venceu3!`;
+    }
+    else if (playerSelection == 3 && (cpuNumber == 1 || cpuNumber == 4)) {
+        return `Venceu4!`;
+    }
+    else if (playerSelection == 4 && (cpuNumber == 2 || cpuNumber == 0)) {
+        return `Venceu5!`;
+    }
+    else  {
+        return `Perdeu!`;
+    }
+}
+
+const playerSelection = 4;
+console.log (playRound())

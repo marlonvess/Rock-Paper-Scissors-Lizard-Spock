bcad240c1535eff78c39f0f21c228cde5780024f
marlonvess <marlonvess@gmail.com>
2023-03-10 12:59:30 +0000

    update game scoreboard

--- javascript.js
+++ javascript.js
@@ -4,33 +4,74 @@ function getComputerChoice () {
  return cpuChoice 
 }
 
-const cpuNumber = getComputerChoice();
 
 // 0 = Rock     1 = Paper   2 = Scissors    3 = Lizard      4 = Spock
 
-function playRound (cpuNumber, playerSelection) {
+//Options based on above numbers, game rules were applied in the conditional below:
+let playerPoints = 0;
+let cpuPoints = 0;
+
+
+function playRound () {
+    let playerSelection;
+    let cpuNumber = getComputerChoice();
+console.log (playerSelection,cpuNumber)
     if (playerSelection == cpuNumber) {
-        return `Empate!`;
+        return `Tie!`;
+        
     } 
+    //Increase player 1 point for victory
     else if (playerSelection == 0 && (cpuNumber == 2 || cpuNumber == 3)) {
-        return `Venceu1!`;
+        ++playerPoints
+        return `You Win!`;
+        
     }
     else if (playerSelection == 1 && (cpuNumber == 4 || cpuNumber == 0)) {
-        return `Venceu2!`;
+        ++playerPoints
+        return `You Win!`;
     }
     else if (playerSelection == 2 && (cpuNumber == 1 || cpuNumber == 3)) {
-        return `Venceu3!`;
+        ++playerPoints
+        return `You Win!`;
+       
     }
     else if (playerSelection == 3 && (cpuNumber == 1 || cpuNumber == 4)) {
-        return `Venceu4!`;
+        ++playerPoints
+        return `You Win!`;
     }
     else if (playerSelection == 4 && (cpuNumber == 2 || cpuNumber == 0)) {
-        return `Venceu5!`;
+        ++playerPoints
+        return `You Win!`;
     }
+    // Increase CPU 1 point
     else  {
-        return `Perdeu!`;
+        ++cpuPoints
+        return `You Lose!`;
+
     }
+
 }
 
-const playerSelection = 4;
-console.log (playRound())
+
+//Player Choice - The Game playeable only via browser console
+
+
+//Show winner:
+function game () {
+ for (let i = 0; i < 5; i++) {
+
+ console.log (playRound())
+ }
+ if (playerPoints > cpuPoints) 
+ {
+    return `You defeat the CPU`
+ } else if (playerPoints < cpuPoints) {
+    return `Bad Ending: The machines took control`
+ }
+ else if (playerPoints = cpuPoints) {
+    return `No winner or loser this time, try again`
+ }
+
+}
+
+console.log (game())
